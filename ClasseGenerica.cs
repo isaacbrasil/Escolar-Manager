fb219275083858b540f5731b59220512a2655078
Isaac <isaac.brasil@escolarmanager.com.br>
2022-10-25 10:07:21 -0300

    removendo código inutilizável

--- ClasseGenerica.cs
+++ ClasseGenerica.cs
@@ -2,23 +2,6 @@
 
 public class ClasseGenerica<T>
 {
-    // private data members
-    private T data;
-
-    // using properties
-    public T value
-    {
-        // using accessors
-        get
-        {
-            return this.data;
-        }
-        set
-        {
-            this.data = value;
-        }
-    }
-
     public string MostraLista(List<T> pessoas)
     {
         if (pessoas.Count > 0)
